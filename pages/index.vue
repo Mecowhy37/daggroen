cac3072dba73c72faf99aa9b29d3c9bafc6ff184
Miko <kozak.mikolaj@gmail.com>
2024-02-21 14:39:20 +0100

    C:/Program Files/Git/pages and sass

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="full">
+    <h1>Welcome to Dag Groen! <span>˶ᵔ ᵕ ᵔ˶</span></h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.full {
+  display: grid;
+  width: 100dvw;
+  height: 100dvh;
+  background-color: #eedfbb;
+  * {
+    place-self: center;
+    color: hsl(44, 60%, 98%);
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+  span {
+    white-space: nowrap;
+  }
+}
+</style>

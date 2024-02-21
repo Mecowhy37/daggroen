4e6bb0e2f3cf7d7619a388fc1fcf052026d03012
Miko <kozak.mikolaj@gmail.com>
2024-02-21 14:42:35 +0100

    /pages and sass

--- app.vue
+++ app.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="full">
-    <h1>Welcome to Dag Groen! <span>˶ᵔ ᵕ ᵔ˶</span></h1>
-  </div>
+  <NuxtPage />
 </template>
 
-<style>
+<style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap");
 
 html,
@@ -15,21 +13,5 @@ html,
   font-family: "Bree Serif", serif;
   font-weight: 400;
   font-style: normal;
-}
-
-.full {
-  display: grid;
-  width: 100dvw;
-  height: 100dvh;
-  background-color: #eedfbb;
-}
-.full > * {
-  place-self: center;
-  color: hsl(44, 60%, 98%);
-  text-align: center;
-  margin-bottom: 2rem;
-}
-.full span {
-  white-space: nowrap;
 }
 </style>

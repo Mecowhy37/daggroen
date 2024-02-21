0084c7c7188e5ad96d570f8d34ce1b235ec9e4fb
Miko <kozak.mikolaj@gmail.com>
2024-02-21 15:00:35 +0100

    scss

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,17 +3,3 @@
     <slot></slot>
   </div>
 </template>
-
-<style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap");
-
-html,
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  font-family: "Bree Serif", serif;
-  font-weight: 400;
-  font-style: normal;
-}
-</style>

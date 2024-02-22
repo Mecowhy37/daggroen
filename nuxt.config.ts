9c190d95821f11674d4417c513c69cae623e0ab5
Miko <kozak.mikolaj@gmail.com>
2024-02-22 13:41:02 +0100

    got it to work with tailwind and prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  features: {
-    inlineStyles: false,
+  modules: [
+    'nuxt-primevue'
+  ],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  primevue: {
+    // unstyled: true
   }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,18 @@
 <template>
-  <div class="full">
-    <h1>Welcome to Dag Groen! <span>˶ᵔ ᵕ ᵔ˶</span></h1>
+  <div class="custom-class grid h-dvh place-content-center text-green-400">
+    <h1 class="text-3xl">
+      Welcome to Dag Groen! <span class="text-nowrap">˶ᵔ ᵕ ᵔ˶</span>
+    </h1>
   </div>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
-.full {
-  display: grid;
-  width: 100dvw;
-  height: 100dvh;
+.custom-class {
   background-color: #eedfbb;
-  * {
-    place-self: center;
-    color: hsl(44, 60%, 98%);
-    text-align: center;
-    margin-bottom: 2rem;
-  }
-  span {
-    white-space: nowrap;
-  }
+}
+h1 {
+  /* @apply text-red-300; */
 }
 </style>
